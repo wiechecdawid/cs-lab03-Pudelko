dcc317c9d65e6f28ff983b4f94bcd07dbcd4162d
wiechecdawid <wiechec.dawid@gmail.com>
2020-11-25 02:43:25 +0100

    sorting

--- Pudelko.CLI/Program.cs
+++ Pudelko.CLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Pudelko.Lib;
 using P = Pudelko.Lib.Pudelko;
 using System;
+using System.Collections.Generic;
 
 namespace Pudelko.CLI
 {
@@ -8,8 +9,32 @@ namespace Pudelko.CLI
     {
         static void Main(string[] args)
         {
-            var p = new P(a: 11.0, b: 2.5, unit: UnitOfMeasure.centimeter);
-            Console.WriteLine("{0} x {1} x {2}", p.A, p.B, p.C);
+            var boxes = new List<P>();
+            var r = new Random();
+
+            for(int i = 0; i < 5; i++)
+            {
+                var p = new P(r.NextDouble() * 10, r.NextDouble() * 10, r.NextDouble() * 10);
+
+                Console.WriteLine($"Wymiary: {p.ToString()} Objetosc: {p.Objetosc} Pole powierzchni: {p.Pole}");
+
+                boxes.Add(p);
+            }
+            Console.WriteLine();
+
+            boxes.Sort((p1, p2) =>
+            {
+                if (p1.Objetosc < p2.Objetosc) return -1;
+                if (p1.Objetosc > p2.Objetosc) return 1;
+                if (p1.Pole < p2.Pole) return -1;
+                if (p1.Pole > p2.Pole) return 1;
+                if (p1.A + p1.B + p1.C < p2.A + p2.B + p2.C) return -1;
+                if (p1.A + p1.B + p1.C > p2.A + p2.B + p2.C) return 1;
+                return 0;
+            });
+
+            foreach(var p in boxes)
+                Console.WriteLine($"Wymiary: {p.ToString()} Objetosc: {p.Objetosc} Pole powierzchni: {p.Pole}");
         }
     }
 }
---
+++ Pudelko.CLI/PudelkoExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using P = Pudelko.Lib.Pudelko;
+
+namespace Pudelko.CLI
+{
+    public static class PudelkoExtensions
+    {
+        public static P Kompresuj(this P pudelko)
+        {
+            double a = Math.Pow(pudelko.Objetosc, 1 / 3);
+
+            return new P(a, a, a);
+        }
+    }
+}
